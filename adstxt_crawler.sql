7fccaea0b88078480ec1491d482cf313c1d86a01
Antoine Jacquemin (Rubicon) <ajacquemin@rubiconproject.com>
2017-10-13 19:08:42 +0800

    Fix
    Remove the primary key which are not unique and raise an error, and the not null from the column "ADSYSTEM_DOMAIN" which is never used.

--- adstxt_crawler.sql
+++ adstxt_crawler.sql
@@ -4,13 +4,12 @@ DROP TABLE IF EXISTS adstxt;
 CREATE TABLE adstxt(
        SITE_DOMAIN                  TEXT    NOT NULL,
        EXCHANGE_DOMAIN              TEXT    NOT NULL,
-       ADSYSTEM_DOMAIN		    INTEGER     NOT NULL,
+       ADSYSTEM_DOMAIN		    INTEGER        ,
        SELLER_ACCOUNT_ID            TEXT    NOT NULL,
        ACCOUNT_TYPE                 TEXT    NOT NULL,
        TAG_ID                       TEXT    NOT NULL,
        ENTRY_COMMENT                TEXT    NOT NULL,
-       UPDATED                      DATE    DEFAULT (datetime('now','localtime')),
-    PRIMARY KEY (SITE_DOMAIN,EXCHANGE_DOMAIN,SELLER_ACCOUNT_ID)
+       UPDATED                      DATE    DEFAULT (datetime('now','localtime'))
 );
 
 -- Contribution by Ian Trider
