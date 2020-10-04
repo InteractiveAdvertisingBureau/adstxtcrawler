e5b58066da14992b636c59403f151741a5933dad
Neal Richter <nrichter@spotx.tv>
2020-10-04 12:56:36 -0600

    manually reverse pull request that deleted the code when adding new results

---
+++ adstxt_crawler.py
@@ -0,0 +1,272 @@
+#!/usr/bin/env python
+
+########################################################################################################
+# Copyright 2017 Hebbian Labs, LLC
+# Copyright 2017 IAB TechLab & OpenRTB Group
+#
+# Author: Neal Richter, neal@hebbian.io
+#
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided
+# that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the
+#    following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+#    the following disclaimer in the documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+########################################################################################################
+
+
+########################################################################################################
+# See README.md file
+#
+# This is a reference implemenation of an ads.txt crawler that downloads, parses and dumps the data to a SQLiteDB
+# The code assumes that you have SQLLite installed and created the DB with the associated SQL file
+#
+# This code would be suitable for a small scale crawler, however it is missing many production features
+# for large scale use, such as parallel HTTP download and parsing of the data files, stateful recovery
+# of target servers being down, usage of a real production DB server etc.  Use as a reference for your own
+# implementations or harden and enhance this code as needed.
+#
+########################################################################################################
+
+import sys
+import csv
+import socket
+import sqlite3
+import logging
+from optparse import OptionParser
+from urlparse import urlparse
+#pip install requests
+import requests
+
+#################################################################
+# FUNCTION process_row_to_db.
+#  handle one row and push to the DB
+#
+#################################################################
+
+def process_row_to_db(conn, data_row, comment, hostname):
+    insert_stmt = "INSERT OR IGNORE INTO adstxt (SITE_DOMAIN, EXCHANGE_DOMAIN, SELLER_ACCOUNT_ID, ACCOUNT_TYPE, TAG_ID, ENTRY_COMMENT) VALUES (?, ?, ?, ?, ?, ? );"
+    exchange_host     = ''
+    seller_account_id = ''
+    account_type      = ''
+    tag_id            = ''
+
+    if len(data_row) >= 3:
+        exchange_host     = data_row[0].lower()
+        seller_account_id = data_row[1].lower()
+        account_type      = data_row[2].lower()
+
+    if len(data_row) == 4:
+        tag_id            = data_row[3].lower()
+
+    #data validation heurstics
+    data_valid = 1;
+
+    # Minimum length of a domain name is 1 character, not including extensions.
+    # Domain Name Rules - Nic AG
+    # www.nic.ag/rules.htm
+    if(len(hostname) < 3):
+        data_valid = 0
+
+    if(len(exchange_host) < 3):
+        data_valid = 0
+
+    # could be single digit integers
+    if(len(seller_account_id) < 1):
+        data_valid = 0
+
+    ## ads.txt supports 'DIRECT' and 'RESELLER'
+    if(len(account_type) < 6):
+        data_valid = 0
+
+    if(data_valid > 0):
+        logging.debug( "%s | %s | %s | %s | %s | %s" % (hostname, exchange_host, seller_account_id, account_type, tag_id, comment))
+
+        # Insert a row of data using bind variables (protect against sql injection)
+        c = conn.cursor()
+        c.execute(insert_stmt, (hostname, exchange_host, seller_account_id, account_type, tag_id, comment))
+
+        # Save (commit) the changes
+        conn.commit()
+        return 1
+
+    return 0
+
+# end process_row_to_db  #####
+
+#################################################################
+# FUNCTION crawl_to_db.
+#  crawl the URLs, parse the data, validate and dump to a DB
+#
+#################################################################
+
+def crawl_to_db(conn, crawl_url_queue):
+
+    rowcnt = 0
+
+    myheaders = {
+            'User-Agent': 'AdsTxtCrawler/1.0; +https://github.com/InteractiveAdvertisingBureau/adstxtcrawler',
+            'Accept': 'text/plain',
+        }
+
+    for aurl in crawl_url_queue:
+        ahost = crawl_url_queue[aurl]
+        logging.info(" Crawling  %s : %s " % (aurl, ahost))
+        r = requests.get(aurl, headers=myheaders)
+        logging.info("  %d" % r.status_code)
+
+        if(r.status_code == 200):
+            logging.debug("-------------")
+            logging.debug(r.request.headers)
+            logging.debug("-------------")
+            logging.debug("%s" % r.text)
+            logging.debug("-------------")
+
+            tmpfile = 'tmpads.txt'
+            with open(tmpfile, 'wb') as tmp_csv_file:
+                tmp_csv_file.write(r.text)
+                tmp_csv_file.close()
+
+            with open(tmpfile, 'rb') as tmp_csv_file:
+                #read the line, split on first comment and keep what is to the left (if any found)
+                line_reader = csv.reader(tmp_csv_file, delimiter='#', quotechar='|')
+                comment = ''
+
+                for line in line_reader:
+                    logging.debug("DATA:  %s" % line)
+
+                    try:
+                        data_line = line[0]
+                    except:
+                        data_line = "";
+
+                    #determine delimiter, conservative = do it per row
+                    if data_line.find(",") != -1:
+                        data_delimiter = ','
+                    elif data_line.find("\t") != -1:
+                        data_delimiter = '\t'
+                    else:
+                        data_delimiter = ' '
+
+                    data_reader = csv.reader([data_line], delimiter=',', quotechar='|')
+                    for row in data_reader:
+
+                        if len(row) > 0 and row[0].startswith( '#' ):
+                            continue
+
+                        if (len(line) > 1) and (len(line[1]) > 0):
+                             comment = line[1]
+
+                        rowcnt = rowcnt + process_row_to_db(conn, row, comment, ahost)
+
+    return rowcnt
+
+# end crawl_to_db  #####
+
+#################################################################
+# FUNCTION load_url_queue
+#  Load the target set of URLs and reduce to an ads.txt domains queue
+#
+#################################################################
+
+def load_url_queue(csvfilename, url_queue):
+    cnt = 0
+
+    with open(csvfilename, 'rb') as csvfile:
+        targets_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in targets_reader:
+
+            if len(row) < 1 or row[0].startswith( '#' ):
+                continue
+
+            for item in row:
+                host = "localhost"
+
+                if  "http:" in item or "https:" in item :
+                    logging.info( "URL: %s" % item)
+                    parsed_uri = urlparse(row[0])
+                    host = parsed_uri.netloc
+                else:
+                    host = item
+                    logging.info( "HOST: %s" % item)
+
+            skip = 0
+
+            try:
+                #print "Checking DNS: %s" % host
+                ip = socket.gethostbyname(host)
+
+                if "127.0.0" in ip:
+                    skip = 0 #swap to 1 to skip localhost testing
+                elif "0.0.0.0" in ip:
+                    skip = 1
+                else:
+                    logging.info("  Validated Host IP: %s" % ip)
+            except:
+                skip = 1
+
+            if(skip < 1):
+                ads_txt_url = 'http://{thehost}/ads.txt'.format(thehost=host)
+                logging.info("  pushing %s" % ads_txt_url)
+                url_queue[ads_txt_url] = host
+                cnt = cnt + 1
+
+    return cnt
+
+# end load_url_queue  #####
+
+#### MAIN ####
+
+arg_parser = OptionParser()
+arg_parser.add_option("-t", "--targets", dest="target_filename",
+                  help="list of domains to crawl ads.txt from", metavar="FILE")
+arg_parser.add_option("-d", "--database", dest="target_database",
+                  help="Database to dump crawled data into", metavar="FILE")
+arg_parser.add_option("-v", "--verbose", dest="verbose", action='count',
+                  help="Increase verbosity (specify multiple times for more)")
+
+(options, args) = arg_parser.parse_args()
+
+if len(sys.argv)==1:
+    arg_parser.print_help()
+    exit(1)
+
+log_level = logging.WARNING # default
+if options.verbose == 1:
+    log_level = logging.INFO
+elif options.verbose >= 2:
+    log_level = logging.DEBUG
+logging.basicConfig(filename='adstxt_crawler.log',level=log_level,format='%(asctime)s %(filename)s:%(lineno)d:%(levelname)s  %(message)s')
+
+crawl_url_queue = {}
+conn = None
+cnt_urls = 0
+cnt_records = 0
+
+cnt_urls = load_url_queue(options.target_filename, crawl_url_queue)
+
+if (cnt_urls > 0) and options.target_database and (len(options.target_database) > 1):
+    conn = sqlite3.connect(options.target_database)
+
+with conn:
+    cnt_records = crawl_to_db(conn, crawl_url_queue)
+    if(cnt_records > 0):
+        conn.commit()
+    #conn.close()
+
+print "Wrote %d records from %d URLs to %s" % (cnt_records, cnt_urls, options.target_database)
+
+logging.warning("Wrote %d records from %d URLs to %s" % (cnt_records, cnt_urls, options.target_database))
+logging.warning("Finished.")
+
---
+++ adstxt_crawler.sql
@@ -0,0 +1,209 @@
+BEGIN TRANSACTION;
+DROP TABLE IF EXISTS adstxt;
+
+CREATE TABLE adstxt(
+       SITE_DOMAIN                  TEXT    NOT NULL,
+       EXCHANGE_DOMAIN              TEXT    NOT NULL,
+       ADSYSTEM_DOMAIN		    INTEGER     NOT NULL,
+       SELLER_ACCOUNT_ID            TEXT    NOT NULL,
+       ACCOUNT_TYPE                 TEXT    NOT NULL,
+       TAG_ID                       TEXT    NOT NULL,
+       ENTRY_COMMENT                TEXT    NOT NULL,
+       UPDATED                      DATE    DEFAULT (datetime('now','localtime')),
+    PRIMARY KEY (SITE_DOMAIN,EXCHANGE_DOMAIN,SELLER_ACCOUNT_ID)
+);
+
+-- Contribution by Ian Trider
+DROP TABLE IF EXISTS adsystem_domain;
+
+CREATE TABLE "adsystem_domain" (
+	DOMAIN	TEXT,
+	ID	INTEGER,
+	PRIMARY KEY(DOMAIN,ID)
+);
+INSERT INTO `adsystem_domain` VALUES ('adtech.com',11);
+INSERT INTO `adsystem_domain` VALUES ('aolcloud.net',11);
+INSERT INTO `adsystem_domain` VALUES ('appnexus.com',84);
+INSERT INTO `adsystem_domain` VALUES ('districtm.io',96);
+INSERT INTO `adsystem_domain` VALUES ('google.com',8);
+INSERT INTO `adsystem_domain` VALUES ('indexechange.com',48);
+INSERT INTO `adsystem_domain` VALUES ('indexexchange.com',48);
+INSERT INTO `adsystem_domain` VALUES ('indexexchnage.com',48);
+INSERT INTO `adsystem_domain` VALUES ('openx.com',4);
+INSERT INTO `adsystem_domain` VALUES ('pubmatic.com',3);
+INSERT INTO `adsystem_domain` VALUES ('rubicon.com',1);
+INSERT INTO `adsystem_domain` VALUES ('rubiconproject.com',1);
+INSERT INTO `adsystem_domain` VALUES ('spotx.tv',44);
+INSERT INTO `adsystem_domain` VALUES ('spotxchange.com',44);
+INSERT INTO `adsystem_domain` VALUES ('spx.smaato.com',17);
+INSERT INTO `adsystem_domain` VALUES ('teads.tv',94);
+INSERT INTO `adsystem_domain` VALUES ('pulsepoint.com',95);
+INSERT INTO `adsystem_domain` VALUES ('aol.com',15);
+INSERT INTO `adsystem_domain` VALUES ('liveintent.com',12);
+INSERT INTO `adsystem_domain` VALUES ('triplelift.com',83);
+INSERT INTO `adsystem_domain` VALUES ('teads.com',94);
+INSERT INTO `adsystem_domain` VALUES ('contextweb.com',95);
+INSERT INTO `adsystem_domain` VALUES ('sharethrough.com',97);
+INSERT INTO `adsystem_domain` VALUES ('districtm.ca',96);
+INSERT INTO `adsystem_domain` VALUES ('sovrn.com',23);
+INSERT INTO `adsystem_domain` VALUES ('smaato.com',17);
+INSERT INTO `adsystem_domain` VALUES ('coxmt.com',86);
+INSERT INTO `adsystem_domain` VALUES ('lijit.com',23);
+INSERT INTO `adsystem_domain` VALUES ('www.indexexchange.com',48);
+INSERT INTO `adsystem_domain` VALUES ('tremorhub.com',77);
+INSERT INTO `adsystem_domain` VALUES ('appnexus',84);
+INSERT INTO `adsystem_domain` VALUES ('advertising.com',88);
+INSERT INTO `adsystem_domain` VALUES ('fastlane.rubiconproject.com',1);
+INSERT INTO `adsystem_domain` VALUES ('33across.com',2);
+INSERT INTO `adsystem_domain` VALUES ('facebook.com',5);
+INSERT INTO `adsystem_domain` VALUES ('gumgum.com',6);
+INSERT INTO `adsystem_domain` VALUES ('kargo.com',7);
+INSERT INTO `adsystem_domain` VALUES ('brealtime.com',9);
+INSERT INTO `adsystem_domain` VALUES ('c.amazon-adsystem.com',10);
+INSERT INTO `adsystem_domain` VALUES ('yieldmo.com',13);
+INSERT INTO `adsystem_domain` VALUES ('taboola.com',18);
+INSERT INTO `adsystem_domain` VALUES ('sofia.trustx.org',19);
+INSERT INTO `adsystem_domain` VALUES ('a9.com',10);
+INSERT INTO `adsystem_domain` VALUES ('amazon.com',10);
+INSERT INTO `adsystem_domain` VALUES ('lkqd.com',20);
+INSERT INTO `adsystem_domain` VALUES ('criteo.com',21);
+INSERT INTO `adsystem_domain` VALUES ('exponential.com',22);
+INSERT INTO `adsystem_domain` VALUES ('yldbt.com',25);
+INSERT INTO `adsystem_domain` VALUES ('rhythmone.com',24);
+INSERT INTO `adsystem_domain` VALUES ('technorati.com',26);
+INSERT INTO `adsystem_domain` VALUES ('bidfluence.com',27);
+INSERT INTO `adsystem_domain` VALUES ('switch.com',28);
+INSERT INTO `adsystem_domain` VALUES ('amazon-adsystem.com',10);
+INSERT INTO `adsystem_domain` VALUES ('conversantmedia.com',30);
+INSERT INTO `adsystem_domain` VALUES ('sonobi.com',31);
+INSERT INTO `adsystem_domain` VALUES ('spoutable.com',32);
+INSERT INTO `adsystem_domain` VALUES ('trustx.org',19);
+INSERT INTO `adsystem_domain` VALUES ('freewheel.tv',33);
+INSERT INTO `adsystem_domain` VALUES ('connatix.com',34);
+INSERT INTO `adsystem_domain` VALUES ('lkqd.net',20);
+INSERT INTO `adsystem_domain` VALUES ('positivemobile.com',36);
+INSERT INTO `adsystem_domain` VALUES ('memeglobal.com',37);
+INSERT INTO `adsystem_domain` VALUES ('kixer.com',38);
+INSERT INTO `adsystem_domain` VALUES ('sekindo.com',39);
+INSERT INTO `adsystem_domain` VALUES ('360yield.com',40);
+INSERT INTO `adsystem_domain` VALUES ('cdn.stickyadstv.com',33);
+INSERT INTO `adsystem_domain` VALUES ('adform.com',41);
+INSERT INTO `adsystem_domain` VALUES ('streamrail.net',45);
+INSERT INTO `adsystem_domain` VALUES ('mathtag.com',46);
+INSERT INTO `adsystem_domain` VALUES ('adyoulike.com',47);
+INSERT INTO `adsystem_domain` VALUES ('kiosked.com',50);
+INSERT INTO `adsystem_domain` VALUES ('video.unrulymedia.com',51);
+INSERT INTO `adsystem_domain` VALUES ('meridian.sovrn.com',23);
+INSERT INTO `adsystem_domain` VALUES ('brightcom.com',52);
+INSERT INTO `adsystem_domain` VALUES ('smartadserver.com',64);
+INSERT INTO `adsystem_domain` VALUES ('apnexus.com',84);
+INSERT INTO `adsystem_domain` VALUES ('jadserve.postrelease.com',56);
+INSERT INTO `adsystem_domain` VALUES ('rs-stripe.com',53);
+INSERT INTO `adsystem_domain` VALUES ('fyber.com',54);
+INSERT INTO `adsystem_domain` VALUES ('inner-active.com',43);
+INSERT INTO `adsystem_domain` VALUES ('tidaltv.com',55);
+INSERT INTO `adsystem_domain` VALUES ('critero.com',21);
+INSERT INTO `adsystem_domain` VALUES ('advertising.amazon.com',10);
+INSERT INTO `adsystem_domain` VALUES ('nativo.com',56);
+INSERT INTO `adsystem_domain` VALUES ('media.net',57);
+INSERT INTO `adsystem_domain` VALUES ('www.yumenetworks.com',58);
+INSERT INTO `adsystem_domain` VALUES ('revcontent.com',59);
+INSERT INTO `adsystem_domain` VALUES ('adtech.net',11);
+INSERT INTO `adsystem_domain` VALUES ('go.sonobi.com',31);
+INSERT INTO `adsystem_domain` VALUES ('outbrain.com',60);
+INSERT INTO `adsystem_domain` VALUES ('ib.adnxs.com',84);
+INSERT INTO `adsystem_domain` VALUES ('freeskreen.com',62);
+INSERT INTO `adsystem_domain` VALUES ('bidtellect.com',63);
+INSERT INTO `adsystem_domain` VALUES ('loopme.com',65);
+INSERT INTO `adsystem_domain` VALUES ('vidazoo.com',66);
+INSERT INTO `adsystem_domain` VALUES ('videoflare.com',67);
+INSERT INTO `adsystem_domain` VALUES ('yahoo.com',68);
+INSERT INTO `adsystem_domain` VALUES ('yume.com',58);
+INSERT INTO `adsystem_domain` VALUES ('pixfuture.com',69);
+
+DROP TABLE IF EXISTS adsystem;
+CREATE TABLE "adsystem" (
+	ID	INTEGER,
+	NAME	TEXT,
+	CANONICAL_DOMAIN	TEXT,
+	PRIMARY KEY(ID)
+);
+INSERT INTO `adsystem` VALUES (1,'Rubicon',NULL);
+INSERT INTO `adsystem` VALUES (2,'33Across',NULL);
+INSERT INTO `adsystem` VALUES (3,'PubMatic','pubmatic.com');
+INSERT INTO `adsystem` VALUES (4,'OpenX','openx.com');
+INSERT INTO `adsystem` VALUES (5,'Facebook',NULL);
+INSERT INTO `adsystem` VALUES (6,'GumGum',NULL);
+INSERT INTO `adsystem` VALUES (7,'Kargo',NULL);
+INSERT INTO `adsystem` VALUES (8,'Google','google.com');
+INSERT INTO `adsystem` VALUES (9,'bRealtime',NULL);
+INSERT INTO `adsystem` VALUES (10,'Amazon',NULL);
+INSERT INTO `adsystem` VALUES (11,'One by AOL: Display',NULL);
+INSERT INTO `adsystem` VALUES (12,'LiveIntent',NULL);
+INSERT INTO `adsystem` VALUES (13,'Yieldmo',NULL);
+INSERT INTO `adsystem` VALUES (14,'MoPub',NULL);
+INSERT INTO `adsystem` VALUES (15,'One by AOL: Mobile','aol.com');
+INSERT INTO `adsystem` VALUES (17,'Smaato',NULL);
+INSERT INTO `adsystem` VALUES (18,'Taboola',NULL);
+INSERT INTO `adsystem` VALUES (19,'TrustX',NULL);
+INSERT INTO `adsystem` VALUES (20,'LKQD',NULL);
+INSERT INTO `adsystem` VALUES (21,'Criteo',NULL);
+INSERT INTO `adsystem` VALUES (22,'Exponential',NULL);
+INSERT INTO `adsystem` VALUES (23,'Sovrn',NULL);
+INSERT INTO `adsystem` VALUES (24,'RhythmOne',NULL);
+INSERT INTO `adsystem` VALUES (25,'Yieldbot',NULL);
+INSERT INTO `adsystem` VALUES (26,'Technorati',NULL);
+INSERT INTO `adsystem` VALUES (27,'Bidfluence',NULL);
+INSERT INTO `adsystem` VALUES (28,'Switch Concepts',NULL);
+INSERT INTO `adsystem` VALUES (29,'BrightRoll Exchange',NULL);
+INSERT INTO `adsystem` VALUES (30,'Conversant',NULL);
+INSERT INTO `adsystem` VALUES (31,'Sonobi',NULL);
+INSERT INTO `adsystem` VALUES (32,'Spoutable',NULL);
+INSERT INTO `adsystem` VALUES (33,'FreeWheel',NULL);
+INSERT INTO `adsystem` VALUES (34,'Connatix',NULL);
+INSERT INTO `adsystem` VALUES (35,'Centro Brand Exchange',NULL);
+INSERT INTO `adsystem` VALUES (36,'Positive Mobile',NULL);
+INSERT INTO `adsystem` VALUES (37,'MemeGlobal',NULL);
+INSERT INTO `adsystem` VALUES (38,'Kixer',NULL);
+INSERT INTO `adsystem` VALUES (39,'Sekindo',NULL);
+INSERT INTO `adsystem` VALUES (40,'Improve Digital','improvedigital.com');
+INSERT INTO `adsystem` VALUES (41,'AdForm',NULL);
+INSERT INTO `adsystem` VALUES (42,'MADS',NULL);
+INSERT INTO `adsystem` VALUES (43,'Inneractive','inner-active.com');
+INSERT INTO `adsystem` VALUES (44,'SpotX',NULL);
+INSERT INTO `adsystem` VALUES (45,'StreamRail',NULL);
+INSERT INTO `adsystem` VALUES (46,'MediaMath',NULL);
+INSERT INTO `adsystem` VALUES (47,'AdYouLike',NULL);
+INSERT INTO `adsystem` VALUES (48,'Index Exchange','indexexchange.com');
+INSERT INTO `adsystem` VALUES (49,'e-Planning',NULL);
+INSERT INTO `adsystem` VALUES (50,'Kiosked',NULL);
+INSERT INTO `adsystem` VALUES (51,'UnrulyX',NULL);
+INSERT INTO `adsystem` VALUES (52,'Brightcom',NULL);
+INSERT INTO `adsystem` VALUES (53,'PowerInbox',NULL);
+INSERT INTO `adsystem` VALUES (54,'Fyber','fyber.com');
+INSERT INTO `adsystem` VALUES (55,'TidalTV',NULL);
+INSERT INTO `adsystem` VALUES (56,'Nativo',NULL);
+INSERT INTO `adsystem` VALUES (57,'Media.net',NULL);
+INSERT INTO `adsystem` VALUES (58,'YuMe',NULL);
+INSERT INTO `adsystem` VALUES (59,'RevContent',NULL);
+INSERT INTO `adsystem` VALUES (60,'Outbrain',NULL);
+INSERT INTO `adsystem` VALUES (61,'Zedo',NULL);
+INSERT INTO `adsystem` VALUES (62,'SlimCut Media',NULL);
+INSERT INTO `adsystem` VALUES (63,'Bidtellect',NULL);
+INSERT INTO `adsystem` VALUES (64,'Smart RTB+',NULL);
+INSERT INTO `adsystem` VALUES (65,'LoopMe',NULL);
+INSERT INTO `adsystem` VALUES (66,'Vidazoo',NULL);
+INSERT INTO `adsystem` VALUES (67,'Videoflare',NULL);
+INSERT INTO `adsystem` VALUES (68,'Yahoo Ad Exchange',NULL);
+INSERT INTO `adsystem` VALUES (69,'PixFuture',NULL);
+INSERT INTO `adsystem` VALUES (77,'Tremor',NULL);
+INSERT INTO `adsystem` VALUES (83,'TripleLift',NULL);
+INSERT INTO `adsystem` VALUES (84,'AppNexus','appnexus.com');
+INSERT INTO `adsystem` VALUES (86,'COMET',NULL);
+INSERT INTO `adsystem` VALUES (88,'One by AOL: Video','advertising.com');
+INSERT INTO `adsystem` VALUES (94,'Teads','teads.tv');
+INSERT INTO `adsystem` VALUES (95,'PulsePoint',NULL);
+INSERT INTO `adsystem` VALUES (96,'District M',NULL);
+INSERT INTO `adsystem` VALUES (97,'Sharethrough',NULL);
+COMMIT;
+
